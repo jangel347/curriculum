0baca6f258d0ca992df45a81b3b5f2fe7789780a
Jeison Angel <jestean347@gmail.com>
2021-01-17 21:31:57 -0500

    Further training is ready

--- libraries/animations.js
+++ libraries/animations.js
@@ -22,3 +22,50 @@ function slider_knowledges() {
     }
     console.log("Hecho")
 }
+var count_slides = 1;
+function slide_vertical(direction) {
+    var element = document.getElementsByClassName("slider")[0];
+    if (count_slides >= 1 && count_slides <= element.getElementsByClassName("study_card").length) {
+        translate_count = (direction) ? translate_count - element.getElementsByClassName("study_card")[0].offsetHeight : translate_count + element.getElementsByClassName("study_card")[0].offsetHeight;
+        count_slides = (direction) ? count_slides + 1 : count_slides - 1;
+        element.style.transform = "translatey(" + translate_count + "px)";
+        state_buttons(element.getElementsByClassName("study_card").length);
+    }
+}
+function state_buttons(max) {
+    switch (count_slides) {
+        case max:
+            var btn_down = document.getElementsByClassName("button_down")[0];
+            btn_down.style = "opacity:0";
+            setTimeout(() => {
+                btn_down.style = "display:none;";
+            }, 500);
+            break;
+        case 1:
+            var btn_up = document.getElementsByClassName("button_up")[0];
+            btn_up.style = "opacity:0";
+            setTimeout(() => {
+                btn_up.style = "display:none;";
+            }, 500);
+            break
+        default:
+            var btn_up = document.getElementsByClassName("button_up")[0];
+            var btn_down = document.getElementsByClassName("button_down")[0];
+            btn_up.style = "display:block;opacity:1";
+            btn_down.style = "display:block;opacity:1";
+            break;
+    }
+    show_study(count_slides);
+}
+
+function show_study(indice) {
+    var element = document.getElementsByClassName("slider")[0];
+    var list_cards = element.getElementsByClassName("study_card");
+    for (let i = 0; i < list_cards.length; i++) {
+        if (i == indice - 1) {
+            list_cards[i].classList.add("show");
+        } else {
+            list_cards[i].classList.remove("show");
+        }
+    }
+}
